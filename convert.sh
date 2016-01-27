2ada8e4acea00056cedabc812a851d6dcecbb418
Peter Crosthwaite <peter.crosthwaite@outlook.com>
2016-01-27 23:30:38 +1000

    Enhanced the WriteLog function Enhanced the removing files ability via a for loop Standardised the looping of files for cleanup and processing mkv files Fixed a bug with duplication of log messages due to the rm command not returning any output if it ran successfully. Removed the reading of stdin from WriteLog as it is not required with how the function is being called.
    TODO : remove the various echo statements used for debugging
         : change the required echo statements to use the WriteLog function (inc. those that we don't want displayed on screen)
         : real life testing with proper parameter values
         : pushover notification
         : re-enable emailing the log file upon completion
         : Only email log file if there was a problem?

--- convert.sh
+++ convert.sh
@@ -3,19 +3,172 @@
 ffprobe="/usr/local/bin/ffprobe"
 ffmpeg="/usr/local/bin/ffmpeg"
 LogFile="/tmp/sab.log"
-RC=""
+RC="-1"
+EmailTo="pcrosthwaite@gmail.com"
 
 function WriteLog {
-  
-  echo $1 >> $LogFile
+echo "i am $# - $1 - ${#1}"
+
+  case $# in
+    1)
+      if [ ${#1} -gt 0 ]
+      then
+        IN="$1"
+      fi
+
+    ;;
+
+    2)
+      if [ ${#2} -gt 0 ]
+      then
+        IN="$2"
+      fi
+    ;;
+
+    *)
+
+      IN="Too many arguments to WriteLog - $#"
+    ;;
+  esac
+
+echo "size is ${#IN}"
+if [ ${#IN} -ne 0 ]; then
+  DateTime=`date "+%d/%m/%Y %H:%M:%S"`
+  echo $DateTime' : '$IN >> "$LogFile"
+
+  if [ "$1" != "-noscreen" ]
+  then
+    echo $DateTime' : '$IN
+  fi
+fi
+
+IN=""
+}
+
+function StartConversion {
+  f="$1"
+  WriteLog "Processing File $f"
+
+  ##  Detect what audio codec is being used:
+  audio=$($ffprobe "$f" 2>&1 | sed -n '/Audio:/s/.*: \([a-zA-Z0-9]*\).*/\1/p' | sed 1q)
+  aopts="-c:a ac3 -b:a 640k"
+
+  ##  Set default video settings:
+  vopts="-c:v copy"
+  #vopts="-c:v libx264 -profile:v high -level 4.0"
+
+  ##  Set default subtitle settings:
+  sopts="-c:s copy"
+
+  WriteLog "Audio detected is $audio"
+
+  case "$audio" in
+        aac|alac|mp3|mp2|ac3 )
+
+            ##  If the audio is one of the MP4-supported codecs
+            WriteLog "No Audio Processing Needed."
+            mv "$f" "$f-1"
+            fname="$CleanNZBName" #`basename "$f" .mkv`
+            WriteLog "Executing $ffmpeg -i '$f-1' -vcodec copy -acodec copy '$OutputDir/$fname.mp4'"
+            $ffmpeg -i "$f-1" -vcodec copy -acodec copy "$OutputDir/$fname.mp4"
+            RC=$?
+            CheckRC $RC $f
+        ;;
+
+        "" )
+          ##  If there is no detected audio stream, don't bother
+          WriteLog "Can't Determine Audio, Skipping $f"
+        ;;
+
+        * )
+
+          ##  anything else, convert
+          mv "$f" "$f-1"
+          fname="$CleanNZBName" #`basename "$f" .mkv`
+          WriteLog "Audio Processing Required"
+          WriteLog "Executing $ffmpeg -y -i '$f-1' -map 0 $sopts $vopts $aopts '$OutputDir/$fname.mp4'"
+          $ffmpeg -hwaccel auto -nostdin -y -i "$f-1" -map 0 $sopts $vopts $aopts "$OutputDir/$fname.mp4" 2>&1
+          RC=$?
+          CheckRC $RC $f
+        ;;
+
+  esac
+}
+
+function ReadFiles {
+
+#  go over all MKVs
+DIR="$1"
+
+Mode="$2"
+
+# failsafe - fall back to current directory
+#[ "$DIR" == "" ] && DIR="."
+
+# save and change IFS
+OLDIFS=$IFS
+IFS=$'\n'
+
+# read all file name into an array
+
+case "$Mode" in
+
+  "CleanUp")
+   fileArray=($(find "$DIR" -type f \( -name "*.db" -or -name "*.nfo" -or -name "*sample*.mkv" -or -name "*Sample*.mkv" -or -name "*SAMPLE*.mkv" \)))
+
+  ;;
+
+  "ProcessMKV")
+    fileArray=($(find "$DIR" -name "*.mkv" -type f))
+  ;;
+
+  *)
+    WriteLog "Invalid Mode passed to ReadFiles - $Mode"
+
+  ;;
+
+esac
+
+# restore it
+IFS=$OLDIFS
+
+# get length of an array
+tLen=${#fileArray[@]}
+echo "len is $tLen"
+
+# use for loop read all filenames
+for (( i=0; i<${tLen}; i++ ));
+do
+  f="${fileArray[$i]}"
+  echo "i is $i"
+
+  case "$Mode" in
+    "CleanUp")
+      #f="/tmp/test/gsdfsdfsd"
+      WriteLog "Deleteing $f"
+      result="$(rm ""$f"" 2>&1)"
+      Ret=$?
+      WriteLog -noscreen "rm completed with [$result] : RC ($Ret)"
+    ;;
+
+    "ProcessMKV")
+      StartConversion "$f"
+    ;;
+
+    *)
+     WriteLog "Not processing any file changes dur to invalid mode"
+    ;;
+  esac
+done
+
 }
 
 function SendNotification {
-  echo "convert.sh has finished" | mutt -a $LogFile -s "File Converted RC=$RC" -- pcrosthwaite@gmail.com 
+  WriteLog "Sending email to $EmailTo"
+#  echo "convert.sh has finished" | mutt -a $LogFile -s "File Converted RC=$RC" -- $EmailTo
 }
 
 function CheckRC {
-  
   case "$1" in
        "0" )
           ##  put new file in place
@@ -31,7 +184,6 @@ function CheckRC {
           mv "$2"-1 "$2"
        ;;
   esac
- 
 }
 
 rm -rf $LogFile
@@ -89,67 +241,24 @@ echo "FailURL............ $FailURL" >> $LogFile
 echo "OutputDir.......... $OutputDir" >> $LogFile
 echo "------------------------" >> $LogFile
 
-##  cleanup by deleting unwanted files
-find "$FinalDir" -name "*.txt" -type f -print0 | xargs -0 rm -rf
-find "$FinalDir" -name "*.db" -type f -print0 | xargs -0 rm -rf
-find "$FinalDir" -name "*.nfo" -type f -print0 | xargs -0 rm -rf
-find "$FinalDir" -name "*sample*.mkv" -type f -print0 | xargs -0 rm -rf
-find "$FinalDir" -name "*Sample*.mkv" -type f -print0 | xargs -0 rm -rf
-find "$FinalDir" -name "*SAMPLE*.mkv" -type f -print0 | xargs -0 rm -rf
+if [ -d "$FinalDir" ]
+then
+  ##  cleanup by deleting unwanted files
+  WriteLog -noscreen "Begin File Cleanup"
 
-##  go over all MKVs
-find "$FinalDir" -name "*.mkv" -type f | while read f
-do
-  WriteLog "Processing File $f"
+  ReadFiles "$FinalDir" "CleanUp"
 
-  ##  Detect what audio codec is being used:
-  audio=$($ffprobe "$f" 2>&1 | sed -n '/Audio:/s/.*: \([a-zA-Z0-9]*\).*/\1/p' | sed 1q)
-  aopts="-c:a ac3 -b:a 640k"
+  WriteLog -noscreen "End File Cleanup"
 
-  ##  Set default video settings:
-  vopts="-c:v copy"
-  #vopts="-c:v libx264 -profile:v high -level 4.0"
+  ReadFiles "$FinalDir" "ProcessMKV"
 
-  ##  Set default subtitle settings:
-  sopts="-c:s copy"
+else
+  WriteLog "$FinalDir doesn't exist bitches"
+fi
 
-  WriteLog "Audio detected is $audio"
-
-  case "$audio" in
-        aac|alac|mp3|mp2|ac3 )
-        
-            ##  If the audio is one of the MP4-supported codecs
-	    WriteLog "No Audio Processing Needed."
-            mv "$f" "$f-1"
-            fname="$CleanNZBName" #`basename "$f" .mkv`
-            WriteLog "Executing $ffmpeg -i '$f-1' -vcodec copy -acodec copy '$OutputDir/$fname.mp4'"
-            $ffmpeg -i "$f-1" -vcodec copy -acodec copy "$OutputDir/$fname.mp4"
-            RC=$?
-            CheckRC $RC $f
-        ;;
-        
-        "" )
-          ##  If there is no detected audio stream, don't bother
-          WriteLog "Can't Determine Audio, Skipping $f"
-        ;;
-        
-        * )
-
-          ##  anything else, convert
-	  mv "$f" "$f-1"
-          fname="$CleanNZBName" #`basename "$f" .mkv`
-          WriteLog "Audio Processing Required"
-          WriteLog "Executing $ffmpeg -y -i '$f-1' -map 0 $sopts $vopts $aopts '$OutputDir/$fname.mp4'"
-          $ffmpeg -hwaccel auto -nostdin -y -i "$f-1" -map 0 $sopts $vopts $aopts "$OutputDir/$fname.mp4" 2>&1
-          RC=$?
-          CheckRC $RC $f
-        ;;
-	
-  esac
-done
-
-echo "Done." >> $LogFile
+WriteLog "Done."
 chmod 666 $LogFile
 SendNotification
 
 exit 0
+
