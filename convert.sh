b8e425ae2b1b7fc2c2669368a04a508b240387aa
Peter Crosthwaite <peter.crosthwaite@outlook.com>
2016-03-06 15:04:22 +1000

    Stopped the final display message from creating an empty logfile.

--- convert.sh
+++ convert.sh
@@ -379,7 +379,7 @@ WriteLog "Executing mv $LogFile `dirname $0`/Logs/"
 mv "$LogFile" "`dirname $0`/Logs/"
 
 # Write to the log and to the screen so ihistory knows my great deeds.
-WriteLog "Processing $ProcessingResult"
+echo "Processing $ProcessingResult"
 
 exit 0
 
